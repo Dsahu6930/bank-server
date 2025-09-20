32b5573a31ccf397d2e2ea59041eed9607324bd6
Deepak sahu <Dsahu6930@gmail.com>
2025-09-20 14:33:06 +0530

    create a userrepo class

--- src/main/resources/UserRepository.java
+++ src/main/resources/UserRepository.java
@@ -1,5 +1,7 @@
+package com.example.springboottutorial.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.springboottutorial.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
-
